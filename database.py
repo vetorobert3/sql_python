54da6b0440cadf54f5caabb3b56ace5c0db4c0cd
Veto Ramirez <vetorobert3@gmail.com>
2021-09-09 20:23:08 -0700

    Created table and added two quotes to the quotes database.

--- database.py
+++ database.py
@@ -1,4 +1,16 @@
 import sqlite3
 
-conn = sqlite3.connect('customer.db')
+conn = sqlite3.connect('quotes.db')
 
+# Create a cursor
+c = conn.cursor()
+
+# Create a table
+c.execute("INSERT INTO quote VALUES ('Buddha', 'You only lose what you cling to.')")
+
+print("Command executed succesfully...")
+# Commit our command
+conn.commit()
+
+# Close our connection
+conn.close()
