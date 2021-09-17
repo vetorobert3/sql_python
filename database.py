b6db5ebda202960844e7353ff77448f987910ab5
Veto Ramirez <vetorobert3@gmail.com>
2021-09-17 10:54:44 -0700

    Playing with formatting in Python.

--- database.py
+++ database.py
@@ -7,8 +7,11 @@ c = conn.cursor()
 
 # Query the database
 c.execute("SELECT * FROM quote")
-
-print(c.fetchall())
+#print(c.fetchone()[1])
+#print(c.fetchmany(2))
+items = c.fetchall()
+for item in items:
+    print(item[0] + ": " + item[1])
 
 #print("Command executed succesfully...")
 # Commit our command
