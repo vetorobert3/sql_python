80e41cb2bf701048a974f02dad015f2436b32a72
Veto Ramirez <vetorobert3@gmail.com>
2021-11-09 19:38:22 -0800

    Worked on functions for the SQLite database with python. Next will be looking into how to make a front end and then deploy.

--- database.py
+++ database.py
@@ -1,22 +1,80 @@
 import sqlite3
 
-conn = sqlite3.connect('quotes.db')
+# Query the database and return all records
+def show_all():
+    # Connect to database
+    conn = sqlite3.connect('quotes.db')
+    # Create a cursor
+    c = conn.cursor()
 
-# Create a cursor
-c = conn.cursor()
+    # Query the database - Order by
+    c.execute("SELECT rowid, * FROM quote")
+    items = c.fetchall()
 
-# Query the database
-c.execute("SELECT * FROM quote WHERE author like 'C%'")
+    for item in items:
+        print(item)
 
-items = c.fetchall()
+    # Commit our command
+    conn.commit()
 
-for item in items:
-    print(item)
+    # Close our connection
+    conn.close()
 
 
-#print("Command executed succesfully...")
-# Commit our command
-conn.commit()
+# Add a new quote
+def add_quote(author, quote):
+    conn = sqlite3.connect('quotes.db')
+    c = conn.cursor()
 
-# Close our connection
-conn.close()
+    c.execute("INSERT INTO quote VALUES (?,?)", (author, quote))
+
+    # Commit our command
+    conn.commit()
+    # Close our connection
+    conn.close()
+
+
+# Delete quote
+def delete_quote(id):
+    conn = sqlite3.connect('quotes.db')
+    c = conn.cursor()
+
+    c.execute("DELETE from quote WHERE rowid = (?)", id)
+
+    # Commit our command
+    conn.commit()
+    # Close our connection
+    conn.close()
+
+
+# Add many quotes
+def add_many_quotes(list):
+    conn = sqlite3.connect('quotes.db')
+    c = conn.cursor()
+
+    c.executemany("INSERT INTO quote VALUES (?,?)", (list))  
+
+    # Commit our command
+    conn.commit()
+    # Close our connection
+    conn.close()
+
+
+# Lookup with Where
+def author_lookup(author):
+    conn = sqlite3.connect('quotes.db')
+    c = conn.cursor()
+
+    c.execute("SELECT rowid, * from quote WHERE author = (?)", (author,))
+    items = c.fetchall()
+
+    for item in items:
+        print(item)
+
+    # Commit our command
+    conn.commit()
+    # Close our connection
+    conn.close()
+
+
+
---
+++ quote_app.py
@@ -0,0 +1,21 @@
+import database     
+
+# Lookup authors
+database.author_lookup("Carl Sagan")
+
+
+# Add a quote
+# database.add_quote("Oliver Wendell Holmes", "All limitations are self-imposed.")
+
+# Add many quotes
+# stuff = [
+#     ("Eileen McDargh", "Determine your priorities and focus on them."),
+#     ("Kurt Cobain", "Wanting to be someone else is a waste of who you are.")
+# ]
+# database.add_many_quotes(stuff)
+
+# Delete quote
+# database.delete_quote("6")
+
+# Show all quotes and authors
+# database.show_all()
